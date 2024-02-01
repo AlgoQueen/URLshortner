fb9ad5a0ffd63a0b00ecf4857ba798e03bcbd4f0
shaihaek <shaiha.ek@gmail.com>
2024-02-01 21:09:31 +0530

    Done without database

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
+import shortuuid
 app = Flask(__name__) #to make the application run on the server
+url_database = {}
 
 @app.route('/') 
 def index():
-    return 'Hello, World!'
+    return render_template('index.html', url_db=url_database)
+
+
+@app.route('/shorten', methods=['POST']) #It is used to update the existing resource or create a new resource. most common HTTP methods.
+def shorten():
+    long_url = request.form['long_url']
+    short_url = generate_short_url()
+    url_database[short_url] = long_url
+    return render_template('index.html', short_url=short_url,url_db=url_database)
+
+
+@app.route('/<short_url>')
+def redirect_to_long_url(short_url):
+    if short_url in url_database:
+        long_url = url_database[short_url]
+        return redirect(long_url)
+    else:
+        return "Short URL not found", 404
+
+@app.route('/')
+def show_all():
+    return render_template('index.html', url_database=url_database)
+
+def generate_short_url():
+    return shortuuid.ShortUUID().random(length=6)
 
 
 if __name__ == '__main__':
