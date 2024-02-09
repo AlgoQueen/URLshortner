04cb7428b5ffab5016572f7ba330312b0f271756
shaihaek <shaiha.ek@gmail.com>
2024-02-09 06:02:53 +0530

    basic url shortner

--- app.py
+++ app.py
@@ -9,52 +9,56 @@ db.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        suggested_name = request.form['suggested_name']
-        long_url = request.form['long_url']
-        if suggested_name and long_url:
-            # Check if the name already exists
-            existing_user = User.query.filter_by(short=suggested_name).first()
-            if existing_user:
-                return 'Error: Name already exists. Please enter a different name.'
-            else:
-                new_user = User(short=suggested_name, long=long_url)
-                db.session.add(new_user)
-                db.session.commit()
-                return 'Thank you for submitting your information!'
-        else:
-            return 'Please enter both your name and place.'
+#     if request.method == 'POST':
+#         suggested_name = request.form['suggested_name']
+#         long_url = request.form['long_url']
+#         if suggested_name and long_url:
+#             # Check if the name already exists
+#             existing_user = User.query.filter_by(short=suggested_name).first()
+#             if existing_user:
+#                 return 'Error: Name already exists. Please enter a different name.'
+#             else:
+#                 new_user = User(short=suggested_name, long=long_url)
+#                 db.session.add(new_user)
+#                 db.session.commit()
+#                 short_url = request.host_url + suggested_name
+#                 #return 'Thank you for submitting your information!'
+#         else:
+#             return 'Please enter both your name and place.'
     return render_template('index.html')
 
 
+@app.route('/shorten', methods=['POST'])
+def shorten_url():
+    long_url = request.form['long_url']
+    suggested_name = request.form['suggested_name']
+
+    # Check if the suggested short URL already exists
+    existing_mapping = User.query.filter_by(short=suggested_name).first()
+    if existing_mapping:
+        return f'The suggested short URL "{suggested_name}" already exists. Please choose a different suggestion.'
+
+    # Store the mapping in the database
+    url_mapping = User(short=suggested_name, long=long_url)
+    db.session.add(url_mapping)
+    db.session.commit()
+
+    short_url = request.host_url + suggested_name
+    return f'Short URL: <a href="{short_url}">{short_url}</a>'
 
 @app.route('/show')
 def show_data():
     links = User.query.all()
     return render_template('show.html', links=links)
 
-# @app.route('/shorten', methods=['POST']) #It is used to update the existing resource or create a new resource. most common HTTP methods.
-# def shorten():
-#     long_url = request.form['long_url']
-#     short_url = generate_short_url()
-#     url_database[short_url] = long_url
-#     return render_template('index.html', short_url=short_url)
 
-
-# @app.route('/<short_url>')
-# def redirect_to_long_url(short_url):
-#     if short_url in url_database:
-#         long_url = url_database[short_url]
-#         return redirect(long_url)
-#     else:
-#         return "Short URL not found", 404
-
-# @app.route('/')
-# def show_all():
-#     return render_template('index.html', url_database=url_database)
-
-# def generate_short_url():
-#     return shortuuid.ShortUUID().random(length=6)
+@app.route('/<short_url>')
+def redirect_to_long_url(short_url):
+    user = User.query.filter_by(short=short_url).first()
+    if user:
+        return redirect(user.long)
+    else:
+        return "Short URL not found", 404
 
 
 if __name__ == '__main__':
